f89eb53c3ced86306ddaab631cf84d7f7f53df51
Dannvrg12 <142259195+Dannvrg12@users.noreply.github.com>
2023-11-22 15:13:11 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='minimal',
+setup(name='N Reinas',
       version='0.1',
       description='A Python package for something',
-      url='http://github.com/rvf0068/minimal',
-      author='Yo Mero',
-      author_email='yomero@gmail.com',
+      url='https://github.com/Dannvrg12/N-Reinas',
+      author='Daniel Vargas',
+      author_email='va401684@uaeh.edu.mx',
       license='MIT',
       packages=['minimal'],
       zip_safe=False)

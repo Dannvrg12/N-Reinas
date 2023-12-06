351a8c13cd22968cf3de32b2a3c3005ff3318fcd
Dannvrg12 <142259195+Dannvrg12@users.noreply.github.com>
2023-12-06 15:24:49 -0600

    Update __init__.py

--- N-Reinas/__init__.py
+++ N-Reinas/__init__.py
@@ -35,3 +35,12 @@ def resolver_n_reinas_util(tablero, fila, n):
 #(soluciones.extend(...)): Las soluciones encontradas en la llamada recursiva se extienden a la lista de soluciones actual. Esto se hace para acumular todas las soluciones válidas.
 
 #Retorno de Soluciones (return soluciones) Finalmente, la función retorna la lista de soluciones encontradas.  
+
+def resolver_n_reinas(n):
+    tablero = [-1] * n
+    return resolver_n_reinas_util(tablero, 0, n)
+
+ #Esta función es la función principal que inicializa el tablero y llama a la función resolver_n_reinas_util con los parámetros iniciales.
+ #Se inicializa una lista llamada tablero con valores -1, indicando que inicialmente no hay reinas colocadas en el tablero.   
+ #La función llama a resolver_n_reinas_util con el tablero inicializado, comenzando desde la fila 0 y especificando el tamaño del tablero.
+ #La función retorna el resultado de la llamada a resolver_n_reinas_util, que es una lista de todas las soluciones encontradas.
